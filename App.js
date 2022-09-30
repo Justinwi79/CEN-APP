8941ee579f7e658f7d631ab98372cfc9308ca404
Justinwi79 <justin.harley@yahoo.com>
2022-09-29 22:16:06 -0400

    update

--- App.js
+++ App.js
@@ -1,56 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, SafeAreaView} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import Menu from './Screens/Menu';
-import Login from './Screens/Login';
-import CreateAccount from './Screens/CreateAccount';
-import Career from './Screens/Career';
-import Advising from './Screens/Advising';
-import Scholarships from './Screens/Scholarships';
-import AMC from './Screens/AMC';
-import Discord from './Screens/Discord';
-import { isLoading, useFonts } from 'expo-font';
-import { Apploading } from 'expo';
+import { useFonts } from 'expo-font';
+import AppLoading from "expo-app-loading";
+import Menu from './src/screens/Menu';
+import Login from './src/screens/Login/Login';
+import CreateAccount from './src/screens/CreateAccount/CreateAccount';
+import Career from './src/screens/Career';
+import Advising from './src/screens/Advising';
+import Scholarships from './src/screens/Scholarships';
+import AMC from './src/screens/AMC';
+import Discord from './src/screens/Discord';
+import Navigation from './src/navigation/index';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   let [fontsLoaded] = useFonts({
     'imprintMTS': require('./assets/fonts/Imprint_MT_Shadow.ttf'),
-    //'MontserratM': require('./assets/fonts/Montserrat-Medium.ttf'),
-    //'MontserratB': require('./assets/fonts/Montserrat-Bold.ttf')
+    'MontserratM': require('./assets/fonts/Montserrat-Medium.ttf'),
+    'MontserratB': require('./assets/fonts/Montserrat-Bold.ttf')
   });
   if(!fontsLoaded){
-    return (<Apploading/>);
+    return (<AppLoading/>);
   }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen options={{ headerShown: false }} name="Menu" component={Menu} />
-        <Stack.Screen options={{ headerShown: true }} name="Login" component={Login} />
-        <Stack.Screen options={{ headerShown: true }} name="CreateAccount" component={CreateAccount} />
-        <Stack.Screen options={{ headerShown: true }} name="Career" component={Career} />
-        <Stack.Screen options={{ headerShown: true }} name="Advising" component={Advising} />
-        <Stack.Screen options={{ headerShown: true }} name="Scholarships" component={Scholarships} />
-        <Stack.Screen options={{ headerShown: true }} name="AMC" component={AMC} />
-        <Stack.Screen options={{ headerShown: true }} name="Discord" component={Discord} />
-
-
-
-      </Stack.Navigator>
-    </NavigationContainer>
+      <Navigation />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
